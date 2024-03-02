42efc380b1411acc67590b875df9600bfa57dee5
Nathanael Ohiro <98163823+Bobyrapsy@users.noreply.github.com>
2024-03-02 21:09:42 +0100

    I add changes here

---
+++ susie.js
@@ -0,0 +1,61 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+const navbar = document.querySelector('.nav-container');
+const secondSection = document.querySelector('.second');
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
+
+
+//Mini Scroll Start
+
+let scrollContainer = document.querySelector(".slide-div");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+ 
+nextBtn.addEventListener("click", ()=>{
+   scrollContainer.style.scrollBehavior = "smooth";
+   scrollContainer.scrollLeft += 450;
+});
+
+backBtn.addEventListener("click", ()=>{
+   scrollContainer.style.scrollBehavior = "smooth";
+   scrollContainer.scrollLeft -= 450;
+});
+
+//Mini Scroll End
+
+window.addEventListener('scroll', function () {
+    const offset = window.scrollY;
+    
+    // Move the second section down as you scroll
+    secondSection.style.transform = `translateY(${offset * -1.7}px)`;
+
+    // Check if the navbar should drop down
+    if (offset > navbar.offsetHeight) {
+        navbar.classList.add('fixed-navbar');
+    } else {
+        navbar.classList.remove('fixed-navbar');
+    }
+});
+
+
+//scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '120px',
+    duration: 2500,
+    delay: 100
+ });
+
+ ScrollReveal().reveal('.first .box-1', {delay:500, origin: 'left'});
+ ScrollReveal().reveal('.first .box-2', {delay:700, origin: 'bottom'});
+ ScrollReveal().reveal('.second .h1', {delay:200, origin: 'left'});
+ ScrollReveal().reveal('.second .sp-one', {delay:200, origin: 'bottom', interval:200 });
+
+
